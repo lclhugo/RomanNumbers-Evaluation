eb85e0acb3f90b7c3b0e924af1d81b77888c4c73
lclhugo <hugo.laclau@gmail.com>
2023-08-02 14:12:22 +0200

    refacto de la partie test

--- RomanNumbers.Test/RomanNumbersTest.cs
+++ RomanNumbers.Test/RomanNumbersTest.cs
@@ -2,15 +2,15 @@ using static RomanNumbers.RomanNumbersConvertor;
 
 namespace RomanNumbers.Test
 {
-    public class UnitTest1
+    public class RomanNumbersTest
     {
-        [Theory(DisplayName = "When I convert 1 to a roman number, then I get 'I'")]
+        [Theory(DisplayName = "When I convert an uint number to roman number, then I get the expected result")]
         [InlineData(1, "I")]
         [InlineData(2, "II")]
         [InlineData(3, "III")]
         [InlineData(4, "IV")]
         [InlineData(5, "V")]
-        public void Test1(uint n, string expected)
+        public void ConvertorTest(uint n, string expected)
         {
             var romanNumber = Convert(n);
             Assert.Equal(expected, romanNumber);
