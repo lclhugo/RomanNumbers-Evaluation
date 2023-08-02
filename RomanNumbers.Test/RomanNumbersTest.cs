305c93e71e5cf29f93bc1c231772833dddc8a8ba
lclhugo <hugo.laclau@gmail.com>
2023-08-02 14:49:29 +0200

    test vert de [1;9]∪[11;19], le 10 est rouge

--- RomanNumbers.Test/RomanNumbersTest.cs
+++ RomanNumbers.Test/RomanNumbersTest.cs
@@ -13,7 +13,16 @@ namespace RomanNumbers.Test
             new object[] { 7, "VII" },
             new object[] { 8, "VIII" },
             new object[] { 9, "IX" },
-            new object[] { 10, "X" }
+            new object[] { 10, "X" },
+            new object[] { 11, "XI" },
+            new object[] { 12, "XII" },
+            new object[] { 13, "XIII" },
+            new object[] { 14, "XIV" },
+            new object[] { 15, "XV" },
+            new object[] { 16, "XVI" },
+            new object[] { 17, "XVII" },
+            new object[] { 18, "XVIII" },
+            new object[] { 19, "XIX" }
         };
 
         [Theory(DisplayName = "When I convert an uint number to a Roman number, then I get the expected result")]
