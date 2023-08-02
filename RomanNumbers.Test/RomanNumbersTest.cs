8e7a8623b70042a5e4617edce6c503656bdb1f7a
lclhugo <hugo.laclau@gmail.com>
2023-08-02 14:29:53 +0200

    refacto de la methode de test car je trouve qu'empiler les InlineData c'est pas très beau

--- RomanNumbers.Test/RomanNumbersTest.cs
+++ RomanNumbers.Test/RomanNumbersTest.cs
@@ -1,58 +1,27 @@
-using static RomanNumbers.RomanNumbersConvertor;
-
 namespace RomanNumbers.Test
 {
     public class RomanNumbersTest
     {
-        [Theory(DisplayName = "When I convert an uint number to roman number, then I get the expected result")]
-        [InlineData(1, "I")]
-        [InlineData(2, "II")]
-        [InlineData(3, "III")]
-        [InlineData(4, "IV")]
-        [InlineData(5, "V")]
+        public static readonly object[][] TestData =
+        {
+            new object[] { 1, "I" },
+            new object[] { 2, "II" },
+            new object[] { 3, "III" },
+            new object[] { 4, "IV" },
+            new object[] { 5, "V" },
+            new object[] { 6, "VI" },
+            new object[] { 7, "VII" },
+            new object[] { 8, "VIII" },
+            new object[] { 9, "IX" },
+            new object[] { 10, "X" }
+        };
+
+        [Theory(DisplayName = "When I convert an uint number to a Roman number, then I get the expected result")]
+        [MemberData(nameof(TestData))]
         public void ConvertorTest(uint n, string expected)
         {
-            var romanNumber = Convert(n);
+            var romanNumber = RomanNumbersConvertor.Convert(n);
             Assert.Equal(expected, romanNumber);
-        }
-
-        [Fact(DisplayName = "When I convert 6 to roman number, then I get VI")]
-        public void TestSix()
-        {
-            var romanNumber = Convert(6);
-            Assert.Equal("VI", romanNumber);
-        }
-
-        [Fact(DisplayName = "When I convert 7 to a Roman number, then I get VII")]
-        public void TestSeven()
-        {
-            var number = 7;
-            var romanNumber = RomanNumbersConvertor.Convert((uint)number);
-            Assert.Equal("VII", romanNumber);
-        }
-
-        [Fact(DisplayName = "When I convert 8 to a Roman number, then I get VIII")]
-        public void TestEight()
-        {
-            var number = 8;
-            var romanNumber = RomanNumbersConvertor.Convert((uint)number);
-            Assert.Equal("VIII", romanNumber);
-        }
-
-        [Fact(DisplayName = "When I convert 9 to a Roman number, then I get IX")]
-        public void TestNine()
-        {
-            var number = 9;
-            var romanNumber = RomanNumbersConvertor.Convert((uint)number);
-            Assert.Equal("IX", romanNumber);
-        }
-
-        [Fact(DisplayName = "When I convert 10 to a Roman number, then I get X")]
-        public void TestTen()
-        {
-            var number = 10;
-            var romanNumber = RomanNumbersConvertor.Convert((uint)number);
-            Assert.Equal("X", romanNumber);
         }
     }
 }
