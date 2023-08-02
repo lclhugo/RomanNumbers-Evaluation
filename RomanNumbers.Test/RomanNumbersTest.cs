0d496215d84c4d85e6c905e8975c1df21c417853
lclhugo <hugo.laclau@gmail.com>
2023-08-02 14:08:43 +0200

    de 1 à 5 ok

--- RomanNumbers.Test/RomanNumbersTest.cs
+++ RomanNumbers.Test/RomanNumbersTest.cs
@@ -6,6 +6,10 @@ namespace RomanNumbers.Test
     {
         [Theory(DisplayName = "When I convert 1 to a roman number, then I get 'I'")]
         [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
         public void Test1(uint n, string expected)
         {
             var romanNumber = Convert(n);
--- RomanNumbers/RomanNumbersConvertor.cs
+++ RomanNumbers/RomanNumbersConvertor.cs
@@ -6,6 +6,10 @@
             => n switch
             {
                 1 => new string('I', 1),
+                2 => new string('I', 2),
+                3 => new string('I', 3),
+                4 => "IV",
+                5 => "V",
                 _ => "Invalid Number"
             };
     }
