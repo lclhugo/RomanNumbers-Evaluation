dd416a9bcf8b3cc41650095a850b93e0037b3fa6
lclhugo <hugo.laclau@gmail.com>
2023-08-02 14:41:09 +0200

    ajout d'un programme console pour executer le convertisseur

--- RomanNumbers.Console/Program.cs
+++ RomanNumbers.Console/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RomanNumbers;
+using System;
+
+namespace RomanNumbersConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number (0 to exit):");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (uint.TryParse(input, out uint number))
+                {
+                    if (number == 0)
+                        break;
+
+                    string romanNumeral = RomanNumbersConvertor.Convert(number);
+                    Console.WriteLine($"Roman numeral: {romanNumeral}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid positive integer or 0 to exit.");
+                }
+            }
+
+            Console.WriteLine("Exit");
+        }
+    }
+}
